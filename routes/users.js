77ff061e0662172d25e383d1a8f800c1099adcac
Juan <kupa.thebest@gmail.com>
2023-08-09 17:04:39 -0300

    crea controladores basicos

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {index} = require("../controllers/userController")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET home page. */
+router.get('/', index);
 
 module.exports = router;
