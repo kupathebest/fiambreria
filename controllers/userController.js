e50aca85a0af47f7135c8bb02e35b071fd234ec1
nath gaitan <nathalia2912dante@gmail.com>
2023-08-09 21:44:12 -0300

    rutas y controladores de login y admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,5 +3,16 @@ module.exports = {
         res.render("user",
          {title: "Fiambreria user"}
          )
+
     },
+    admin:(req, res) =>{
+        res.render("admin",
+        {title:"bienvenido,administrador"}
+        )
+    },
+    login:(req, res) =>{
+        res.render("login",
+        {title:"login panel"}
+        )
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {index} = require("../controllers/userController")
+const {index,login,admin} = require("../controllers/userController")
 
 /* GET home page. */
 router.get('/', index);
+router.get('/login',login);
+router.get('/admin',admin);
 
 module.exports = router;
