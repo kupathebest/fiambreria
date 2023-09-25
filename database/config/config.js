6282e96d67cee4e046adbdd8caf8ea20a066a702
Juan <kupa.thebest@gmail.com>
2023-09-25 12:21:36 -0300

    agrega variable de entorno

--- database/config/config.js
+++ database/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports={
   development: {
-    username: "root",
-    password: "root",
-    database: "fiambre_db",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
     host: "127.0.0.1",
     dialect: "mysql"
   },
